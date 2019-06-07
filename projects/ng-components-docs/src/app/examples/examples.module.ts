4cf8df638370caf91140b93219afd4cc55eaf075
Salihu Abdullahi <S4060B@corp1.ad1.seb.net>
2019-06-07 16:28:31 +0800

    refactor(loader-component): revert and remove unnecessary files

--- projects/ng-components-docs/src/app/examples/examples.module.ts
+++ projects/ng-components-docs/src/app/examples/examples.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonsModule } from './components/buttons/buttons.module';
 import { ModalModule } from './components/modal/modal.module';
-import { LoadersModule } from './components/loaders/loader.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     ButtonsModule,
-    ModalModule,
-    LoadersModule
+    ModalModule
   ]
 })
 export class ExamplesModule { }
