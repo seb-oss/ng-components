17e3d01d73dd481abda665c03d8cff7a6cceff9f
Nuru <nuruddeensalihu@yahoo.com>
2020-04-14 18:17:15 +0800

    feat(pagination): first commit for pagination

---
+++ lib/src/pagination/pagination.component.ts
@@ -0,0 +1,80 @@
+import { Component, Input, OnChanges, SimpleChanges, Output, EventEmitter } from "@angular/core";
+
+@Component({
+    selector: "sebng-pagination",
+    templateUrl: "./pagination.component.html",
+    styleUrls: ["./pagination.component.scss"],
+})
+export class PaginationComponent implements OnChanges {
+    @Input() size: number;
+    @Output() onChange: EventEmitter<any> = new EventEmitter();
+    @Input() value: number;
+
+    @Input() className?: string;
+    @Input() firstText?: string;
+    @Input() id?: string;
+    @Input() lastText?: string;
+    @Input() nextText?: string;
+    @Input() offset?: number;
+    @Input() pagingLength?: number;
+    @Input() previousText?: string;
+
+    @Input() useDotNav?: boolean;
+    @Input() useFirstAndLast?: boolean;
+    @Input() useTextNav?: boolean;
+
+    list: Array<number> = [];
+    dotnavList: Array<number> = [];
+    pagingSize: number = 0;
+
+    setPagingSize() {
+        const initialOffset: number = this.offset ? this.offset : 10;
+        this.pagingSize = Math.ceil(Number(this.size) / initialOffset);
+        this.generateList();
+    }
+    /**
+     * Generates an array of the pages that needs to be displayed
+     * It depends on the size, offset, and the current value
+     * @param {number} value The current value. The current page.
+     * @param {number} length The length of the pagination, number of pages to be displayed.
+     * @returns void
+     */
+    generateList(): void {
+        const genList: Array<number> = [];
+        const length: number = this.pagingLength ? this.pagingLength : 5;
+
+        for (let i = 1; i <= this.pagingSize; i++) {
+            genList.push(i);
+        }
+
+        const medianValue: number = Math.ceil(length / 2);
+        let start: number = 0;
+        let end: number = this.pagingSize;
+
+        if (length < this.pagingSize) {
+            if (this.pagingSize - this.value < medianValue) {
+                start = this.pagingSize - length;
+            } else if (this.value - medianValue > -1) {
+                start = this.value - medianValue;
+            }
+
+            end = start + length;
+        }
+        this.dotnavList = genList;
+        this.list = genList.slice(start, end);
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes.offset || changes.size) {
+            this.setPagingSize();
+        }
+
+        if (changes.pagingLength || changes.value) {
+            this.generateList();
+        }
+    }
+
+    handleOnChange(value: number): void {
+        this.onChange.emit(value);
+    }
+}
---
+++ projects/ng-components-docs/src/app/examples/components/pagination/examples/pagination/pagination.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-pagination",
+    templateUrl: "./pagination.component.html",
+})
+export class PaginationComponent {
+    constructor() {
+        this.setPagination = this.setPagination.bind(this);
+    }
+
+    private _pagination: number = 0;
+    get pagination(): number {
+        return this._pagination;
+    }
+
+    set pagination(value: number) {
+        this._pagination = value;
+    }
+
+    setPagination(value: number) {
+        console.log("Setting pagination here ", value);
+        this.pagination = value;
+    }
+}
