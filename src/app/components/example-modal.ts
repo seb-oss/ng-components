a529f7bf61a32783274dcfedadae566a75772201
Mario <mario.subotic@gmail.com>
2020-04-16 17:03:24 +0800

    refactor(project): removes unused src folder

--- src/app/components/example-modal.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Inject } from "@angular/core";
-import { SebModalRef, SEB_MODAL_DATA } from "@sebgroup/ng-components";
-
-@Component({
-    templateUrl: "example.modal.html",
-})
-export class ExampleModalComponent {
-    constructor(public modal: SebModalRef, @Inject(SEB_MODAL_DATA) public data: any) {}
-}
