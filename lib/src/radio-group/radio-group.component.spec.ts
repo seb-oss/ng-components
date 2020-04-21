6e85e871b8674e5333d08a024332f19ee33ddd02
Mario <mario.subotic@gmail.com>
2020-04-21 16:05:54 +0800

    test(radio-group): adds basic unit tests

---
+++ lib/src/radio-group/radio-group.component.spec.ts
@@ -0,0 +1,73 @@
+import { ViewChild, Component, DebugElement } from "@angular/core";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+import { RadioGroupComponent, RadioGroupItem } from "./radio-group.component";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+
+@Component({
+    selector: "test-sebng-radio-group",
+    template: `
+        <sebng-radio-group
+            id="myRadioGroup"
+            [list]="list"
+            [label]="label"
+            [className]="className"
+            [disabled]="disabled"
+            [condensed]="condensed"
+            [inline]="inline"
+        ></sebng-radio-group>
+    `,
+})
+class RadioGroupTestComponent {
+    @ViewChild(RadioGroupComponent) radioGroupComponent: RadioGroupComponent;
+    list: Array<RadioGroupItem>;
+    label?: string;
+    className?: string;
+    disabled?: boolean = false;
+    condensed?: boolean = false;
+    inline?: boolean = false;
+
+    constructor() {
+        this.list = [
+            { label: "Nigeria", value: "NG", key: "1" },
+            { label: "Malaysia", value: "MY", key: "2" },
+        ];
+    }
+}
+
+describe("RadioGroupComponent", () => {
+    let component: RadioGroupTestComponent;
+    let fixture: ComponentFixture<RadioGroupTestComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [FormsModule, CommonModule],
+            declarations: [RadioGroupComponent, RadioGroupTestComponent],
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(RadioGroupTestComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
+
+    it("Should render label", async(() => {
+        component.label = "Country";
+        fixture.detectChanges();
+        const label = fixture.debugElement.query(By.css(".radio-group-label > b"));
+        expect(label).not.toBeNull();
+        expect(label.nativeElement.innerHTML).toEqual("Country");
+    }));
+
+    it("Should pass custom className prop to component", async(() => {
+        component.className = "custom-class";
+        fixture.detectChanges();
+        expect(fixture.debugElement.query(By.css(".custom-class"))).toBeTruthy();
+    }));
+});
