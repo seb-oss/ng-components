77ed1e1ed38e32810202457b5d35d9931a14f1b2
Mario <mario.subotic@gmail.com>
2020-04-21 17:57:04 +0800

    test(radio-group): add more tests

--- lib/src/radio-group/radio-group.component.spec.ts
+++ lib/src/radio-group/radio-group.component.spec.ts
@@ -11,6 +11,7 @@ import { CommonModule } from "@angular/common";
         <sebng-radio-group
             id="myRadioGroup"
             [list]="list"
+            [(ngModel)]="selectedItem"
             [label]="label"
             [className]="className"
             [disabled]="disabled"
@@ -22,6 +23,7 @@ import { CommonModule } from "@angular/common";
 class RadioGroupTestComponent {
     @ViewChild(RadioGroupComponent) radioGroupComponent: RadioGroupComponent;
     list: Array<RadioGroupItem>;
+    selectedItem: RadioGroupItem;
     label?: string;
     className?: string;
     disabled?: boolean = false;
@@ -69,5 +71,34 @@ describe("RadioGroupComponent", () => {
         component.className = "custom-class";
         fixture.detectChanges();
         expect(fixture.debugElement.query(By.css(".custom-class"))).toBeTruthy();
+    }));
+
+    it("Testing two-way data binding", async () => {
+        const ngModelChange = spyOn(component.radioGroupComponent, "writeValue");
+        component.selectedItem = component.list[1];
+        fixture.detectChanges();
+        await fixture.whenStable().then(() => expect(ngModelChange).toHaveBeenCalled());
+    });
+
+    it("Should disable the element when disabled prop is set to true", async(() => {
+        component.disabled = true;
+        fixture.detectChanges();
+        const container: DebugElement = fixture.debugElement.query(By.css(".disabled"));
+        const elements: DebugElement[] = fixture.debugElement.queryAll(By.css(".custom-control.custom-radio > input:disabled"));
+        expect(container).toBeDefined();
+        expect(container).not.toBeNull();
+        expect(elements).toBeDefined();
+        expect(elements).toBeTruthy();
+        expect(elements.length).toBe(component.list.length);
+    }));
+
+    it("Should select the item when clicked on it", async(() => {
+        const elements: DebugElement[] = fixture.debugElement.queryAll(By.css(".custom-control.custom-radio > input"));
+        expect(elements).toBeDefined();
+        expect(elements).toBeTruthy();
+        expect(elements.length).toBe(component.list.length);
+
+        // TODO: simulate a click on first element
+        // TODO: check to see if .custom-control.custom-radio.selected > input equals 1
     }));
 });
