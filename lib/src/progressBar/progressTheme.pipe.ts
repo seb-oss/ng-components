3a03e857eb5969a599df0e0be68858831f212fb1
Nuru Salihu <nuruddeensalihu@yahoo.com>
2020-06-02 12:16:41 +0800

    Update lib/src/progressBar/progressTheme.pipe.ts
    Spelling error
    
    Co-authored-by: Mario Subotic <49055952+mario-subo@users.noreply.github.com>

--- lib/src/progressBar/progressTheme.pipe.ts
+++ lib/src/progressBar/progressTheme.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from "@angular/core";
 import { ProggressTheme } from "./progressBar.component";
 
-@Pipe({ name: "proggressTheme" })
+@Pipe({ name: "progressTheme" })
 export class ProgressThemePipe implements PipeTransform {
     transform(value: ProggressTheme) {
         switch (value) {
