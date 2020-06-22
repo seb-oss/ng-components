69875e1ca923ed82dc757877d4114eecb79b619b
Chang <S2721C@corp1.ad1.seb.net>
2020-06-22 15:19:53 +0800

    fix(tooltip): fix directive export issue

--- lib/src/tooltip/index.ts
+++ lib/src/tooltip/index.ts
@@ -1,3 +1,4 @@
 export * from "./tooltip.module";
 export * from "./tooltip.component";
+export * from "./tooltip.directive";
 export { TooltipPosition, TooltipTheme, TooltipTrigger } from "./tooltip-content/tooltip-content.component";
