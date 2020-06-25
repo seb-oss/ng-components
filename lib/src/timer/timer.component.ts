8ecf03b275f42c400975d43fd21d9a3f85451204
Nuru <nuruddeensalihu@yahoo.com>
2020-06-25 23:38:57 +0800

    feat(timer): fix timer, iuse rxjs instead

--- lib/src/timer/timer.component.ts
+++ lib/src/timer/timer.component.ts
@@ -10,77 +10,50 @@ import {
     SimpleChanges,
     ChangeDetectorRef,
 } from "@angular/core";
+import { timer, Observable } from "rxjs";
+import { map, takeWhile, tap, finalize } from "rxjs/operators";
 
 @Component({
     selector: "sebng-timer",
     templateUrl: "./timer.component.html",
     encapsulation: ViewEncapsulation.None,
 })
-export class TimerComponent implements OnInit, OnChanges, OnDestroy {
+export class TimerComponent implements OnInit, OnChanges {
     @Input() id?: string;
     @Input() className?: string;
-    @Output() callback = new EventEmitter<KeyboardEvent>();
+    @Output() callback = new EventEmitter<void>();
     @Input() duration: number;
-    public timer: string;
-    private innerInterval: NodeJS.Timeout;
+    public timer: Observable<string>;
 
-    constructor(private changeRef: ChangeDetectorRef) {
-        this.timer = "00:00";
-    }
+    constructor(private changeRef: ChangeDetectorRef) {}
 
-    setTimerValue(timeout: number, callback: Function): void {
-        this.timer = this.convertMStoTime(timeout);
-        return callback();
-    }
-
-    startInterval(timeout: number): void {
-        this.setTimerValue(timeout, () => {
-            this.clearInterval();
-            this.innerInterval = setInterval(() => {
-                if (timeout > 0) {
-                    timeout = timeout - 1000;
-                    this.setTimerValue(timeout, () => {
-                        if (timeout === 0) {
-                            this.callback.emit();
-                            this.clearInterval();
-                        }
-                    });
-                }
-            }, 1000);
-        });
-    }
-
-    clearInterval(): void {
-        if (this.innerInterval) {
-            clearTimeout(this.innerInterval);
-            this.innerInterval = null;
-        }
-    }
-
-    convertMStoTime(value: number): string {
-        const date: Date = new Date(value);
-        return (
-            (date.getUTCHours() > 0 ? date.getUTCHours() + ":" : "") +
-            (date.getUTCMinutes() < 10 ? "0" + date.getUTCMinutes() : date.getUTCMinutes()) +
-            ":" +
-            (date.getUTCSeconds() < 10 ? "0" + date.getUTCSeconds() : date.getUTCSeconds())
+    setTimerValue(): void {
+        this.timer = timer(0, 1000).pipe(
+            map(i => this.duration / 1000 - i),
+            takeWhile(i => i >= 0),
+            map((n: number) => {
+                const date: Date = new Date(n * 1000);
+                return (
+                    (date.getUTCHours() > 0 ? date.getUTCHours() + ":" : "") +
+                    (date.getUTCMinutes() < 10 ? "0" + date.getUTCMinutes() : date.getUTCMinutes()) +
+                    ":" +
+                    (date.getUTCSeconds() < 10 ? "0" + date.getUTCSeconds() : date.getUTCSeconds())
+                );
+            }),
+            finalize(() => this.callback && this.callback.emit())
         );
     }
 
     ngOnInit() {
         if (this.duration !== null && this.duration !== undefined) {
-            this.startInterval(this.duration);
+            this.setTimerValue();
         }
-    }
-
-    ngOnDestroy() {
-        this.clearInterval();
     }
 
     ngOnChanges(changes: SimpleChanges) {
         if (changes.duration) {
             if (changes.duration.currentValue !== null && changes.duration.currentValue !== undefined) {
-                this.startInterval(this.duration);
+                this.setTimerValue();
             }
         }
     }
