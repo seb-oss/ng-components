f8286f852e6068f10abe7fcc69067179bd405666
Omar VodiaK <omarboudoust@MacBook-Pro.local>
2020-07-07 00:59:41 +0800

    feat: add comments for documentation

--- lib/src/checkBox/checkBox.component.ts
+++ lib/src/checkBox/checkBox.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, forwardRef, ViewEncapsulation, OnInit, TemplateRef, Output, EventEmitter, Provider } from "@angular/core";
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from "@angular/forms";
 import { randomId } from "@sebgroup/frontend-tools/dist/randomId";
+import { NgClass } from "@angular/common";
 
 export const CUSTOM_CHECKBOX_CONTROL_VALUE_ACCESSOR: Provider = {
     provide: NG_VALUE_ACCESSOR,
@@ -16,13 +17,21 @@ export const CUSTOM_CHECKBOX_CONTROL_VALUE_ACCESSOR: Provider = {
     encapsulation: ViewEncapsulation.None,
 })
 export class CheckBoxComponent implements ControlValueAccessor {
+    /** Checkbox required label, supported types: `string`, `TemplateRef<HTMLElement>` */
     @Input() label: string | TemplateRef<HTMLElement>;
+    /** Checbox optional id, supported type: `string`, default random string value will be set if not provided */
     @Input() _id?: string = randomId("checkbox-");
+    /** Checkbox optional name, supported type: `string` */
     @Input() name?: string;
-    @Input() className?: string;
-    @Input() disabled?: boolean;
+    /** Checkbox optional className, supported type: `ngClass` */
+    @Input() className?: NgClass;
+    /** Checkbox optional disabled state, supported type: `boolean`, default value: `false`*/
+    @Input() disabled?: boolean = false;
+    /** Checkbox optional description, supported types: `string`, `TemplateRef<HTMLElement>` */
     @Input() description?: string | TemplateRef<HTMLElement>;
+    /** Checkbox optional error, supported types: `string`, `TemplateRef<HTMLElement>` */
     @Input() error?: string | TemplateRef<HTMLElement>;
+    /** Checkbox optional onChange event emitter */
     @Output() onChange?: EventEmitter<boolean> = new EventEmitter<boolean>();
 
     private innerValue: boolean = false;
