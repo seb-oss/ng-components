a5ed8825aee4ba4e54dea7fdab94b9d034475a53
Salihu Abdullahi <S4060B@corp1.ad1.seb.net>
2020-07-08 17:45:17 +0800

    feat(button): fix button unit test

--- lib/src/button/button.component.spec.ts
+++ lib/src/button/button.component.spec.ts
@@ -22,15 +22,16 @@ type ButtonTestItem<T, K> = { value: T; expected: K };
             [theme]="theme"
             (onClick)="onClick($event)"
         >
-            Button
-
-            <span class="ml-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" id="my-svg">
-                    <path
-                        d="M12.8 371.2L.2 485.3c-1.7 15.3 11.2 28.2 26.5 26.5l114.2-12.7 352.4-352.4c25-25 25-65.5 0-90.5l-37.5-37.5c-25-25-65.5-25-90.5 0L12.8 371.2zm113.3 97.4L33 478.9l10.3-93.1 271.9-271.9 82.7 82.7-271.8 272zm344.5-344.5L420.7 174 338 91.3l49.9-49.9c12.5-12.5 32.7-12.5 45.3 0l37.5 37.5c12.4 12.4 12.4 32.7-.1 45.2z"
-                    />
-                </svg>
-            </span>
+            <ng-container button>
+                Button
+                <span class="ml-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" id="my-svg">
+                        <path
+                            d="M12.8 371.2L.2 485.3c-1.7 15.3 11.2 28.2 26.5 26.5l114.2-12.7 352.4-352.4c25-25 25-65.5 0-90.5l-37.5-37.5c-25-25-65.5-25-90.5 0L12.8 371.2zm113.3 97.4L33 478.9l10.3-93.1 271.9-271.9 82.7 82.7-271.8 272zm344.5-344.5L420.7 174 338 91.3l49.9-49.9c12.5-12.5 32.7-12.5 45.3 0l37.5 37.5c12.4 12.4 12.4 32.7-.1 45.2z"
+                        />
+                    </svg>
+                </span>
+            </ng-container>
         </sebng-button>
     `,
 })
@@ -111,8 +112,6 @@ describe("ButtonComponent", () => {
             it(`Size: ${item.value} - Expected to render (btn-${item.expected})`, () => {
                 component.size = item.value;
                 fixture.detectChanges();
-
-                console.log("Feed the fire ", fixture.debugElement.query(By.css("button")).classes);
 
                 expect(fixture.debugElement.query(By.css("button")).classes[item.expected]).toBeTrue();
             });
