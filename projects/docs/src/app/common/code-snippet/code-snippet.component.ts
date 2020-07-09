5a6c6c145cf111c1b4a0af5b68a46ef8bef1c075
Chang, Kher Phay <kherphay.chang@seb.se>
2020-07-09 17:11:33 +0800

    add api parser

--- projects/docs/src/app/common/code-snippet/code-snippet.component.ts
+++ projects/docs/src/app/common/code-snippet/code-snippet.component.ts
@@ -1,10 +1,18 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import { APIExtractService } from "../services/api-extract.service";
 
 @Component({
   selector: "app-code-snippet",
   templateUrl: "./code-snippet.component.html",
   styleUrls: ["./code-snippet.component.scss"]
 })
-export class CodeSnippetComponent {
+export class CodeSnippetComponent implements OnInit {
   @Input() lang?: string = "";
+
+  constructor(private apiSrv: APIExtractService) {
+  }
+
+  ngOnInit() {
+    this.apiSrv.initParse("../footer/footer.component.ts");
+  }
 }
