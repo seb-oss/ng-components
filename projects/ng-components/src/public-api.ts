d293836986e27b1100c46dbe655af5804263d899
Yousif Al-Raheem <yousifalraheem@gmail.com>
2020-07-09 12:41:23 +0800

    cleanup

--- projects/ng-components/src/public-api.ts
+++ projects/ng-components/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of ng-components
  */
 
-export * from './lib/ng-components.service';
-export * from './lib/ng-components.component';
-export * from './lib/ng-components.module';
+export * from "./lib/button/button.module";
