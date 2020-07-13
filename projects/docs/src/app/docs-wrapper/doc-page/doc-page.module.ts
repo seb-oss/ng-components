cca9bb76b48854b2e0a58452102a25287701cd59
Yousif Al-Raheem <yousifalraheem@gmail.com>
2020-07-13 18:47:13 +0800

    feat: fixed controls, notes, code snippet and more

--- projects/docs/src/app/docs-wrapper/doc-page/doc-page.module.ts
+++ projects/docs/src/app/docs-wrapper/doc-page/doc-page.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from "@angular/core";
 import { TabsModule } from "@sebgroup/ng-components/tabs";
-import { PlaygroundComponent } from "./playground/playground.component";
 import { DocPageComponent } from "./doc-page.component";
 import { CommonModule } from "@angular/common";
 import { APIExtractService } from "../../common/services/api-extract.service";
-import { ControlsComponent } from "./controls/controls.component";
 import { APIsComponent } from "./apis/apis.component";
 import { FormsModule } from "@angular/forms";
+import { PlaygroundComponent } from "./playground/playground.component";
+import { DocNotesComponent } from "./notes/notes.component";
 
 @NgModule({
-    declarations: [DocPageComponent, PlaygroundComponent, ControlsComponent, APIsComponent],
+    declarations: [DocPageComponent, PlaygroundComponent, APIsComponent, DocNotesComponent],
     imports: [CommonModule, FormsModule, TabsModule],
     exports: [DocPageComponent],
     providers: [APIExtractService],
