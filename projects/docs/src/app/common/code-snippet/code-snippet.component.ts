b68bcb4abd94176a963a733a01c2006ac58510ce
Yousif Al-Raheem <yousifalraheem@gmail.com>
2020-07-14 10:51:02 +0800

    feat: added fill mode for code snippets

--- projects/docs/src/app/common/code-snippet/code-snippet.component.ts
+++ projects/docs/src/app/common/code-snippet/code-snippet.component.ts
@@ -1,14 +1,17 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, ViewEncapsulation, HostBinding } from "@angular/core";
 
 @Component({
     selector: "app-code-snippet",
     template: `
-        <code [lang]="lang">
+        <code class="code-snippet" [class.fill]="fillMode" [lang]="lang">
             <ng-content></ng-content>
         </code>
     `,
-    styleUrls: ["./code-snippet.component.scss"]
+    styleUrls: ["./code-snippet.component.scss"],
+    encapsulation: ViewEncapsulation.None,
 })
 export class CodeSnippetComponent {
     @Input() lang?: string = "";
+    @Input() fillMode?: boolean = false;
+    @HostBinding("class") classes = "w-100";
 }
