c5e1965f1de1504a69ec7bad1c9c59efb07494c5
kp <kayphay92@gmail.com>
2020-07-14 19:20:33 +0800

    feat(video): add new control in video doc

--- projects/docs/src/app/docs-wrapper/pages/video-page/video-page.component.ts
+++ projects/docs/src/app/docs-wrapper/pages/video-page/video-page.component.ts
@@ -1,30 +1,42 @@
 import { Component, OnInit } from "@angular/core";
 import { VideoSourceType } from "@sebgroup/ng-components/video";
+import { DropdownItem } from "@sebgroup/ng-components/dropdown";
 
 @Component({
     selector: "app-video-page",
     templateUrl: "./video-page.component.html",
 })
 export class VideoPageComponent implements OnInit {
+    private defaultSrc: string = require("file-loader!../../../../assets/videos/sample.mp4").default;
+    private _src: string = this.defaultSrc;
     importString: string = require("!raw-loader!@sebgroup/ng-components/video/video.component");
+    code: string = `<sebng-video [src]="src" [sourceType]="sourceType" [width]="width" [height]="height"></sebng-video>`;
 
-    allowFullScreen?: boolean = true;
-    autoplay?: boolean = true;
-    className?: string;
+    allowFullScreen: boolean = true;
+    autoplay: boolean = true;
+    className: string;
     height: number = 300;
-    id?: string;
-    loop?: boolean = true;
+    id: string;
+    loop: boolean = true;
     name: string = "myVideo";
-    showControls?: boolean = true;
-    showInfo?: boolean = true;
-    sourceType: VideoSourceType = "local";
+    showControls: boolean = true;
+    showInfo: boolean = true;
     width: number = 535;
 
-    src: string = require("file-loader!../../../../assets/videos/sample.mp4").default;
-
+    get src(): string {
+        return this._src;
+    }
+    set src(newSrc: string) {
+        this._src = newSrc || this.defaultSrc;
+    }
+    sourceTypeList: Array<DropdownItem> = [
+        { value: "local", label: "local", key: "local" },
+        { value: "stream", label: "stream", key: "stream" },
+    ];
+    sourceType: DropdownItem = this.sourceTypeList[0];
     constructor() {
-        document.title = "Accordion - SEB Angular Components";
+        document.title = "Video - SEB Angular Components";
     }
 
-    ngOnInit(): void { }
+    ngOnInit(): void {}
 }
--- projects/docs/src/app/docs-wrapper/pages/video-page/video-page.module.ts
+++ projects/docs/src/app/docs-wrapper/pages/video-page/video-page.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from "@angular/core";
 import { VideoPageComponent } from "./video-page.component";
 import { VideoModule } from "@sebgroup/ng-components/video";
+import { DropdownModule } from "@sebgroup/ng-components/dropdown";
+import { CheckBoxModule } from "@sebgroup/ng-components/checkBox";
+import { TextboxGroupModule } from "@sebgroup/ng-components/textboxGroup";
 import { CommonModule } from "@angular/common";
 import { Routes, RouterModule } from "@angular/router";
 import { DocPageModule } from "../../doc-page/doc-page.module";
@@ -10,7 +13,16 @@ const routes: Routes = [{ path: "", component: VideoPageComponent }];
 
 @NgModule({
     declarations: [VideoPageComponent],
-    imports: [CommonModule, RouterModule.forChild(routes), FormsModule, DocPageModule, VideoModule],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+        FormsModule,
+        DocPageModule,
+        VideoModule,
+        DropdownModule,
+        TextboxGroupModule,
+        CheckBoxModule,
+    ],
     exports: [RouterModule, VideoModule],
 })
-export class VideoPageModule { }
+export class VideoPageModule {}
