aca1180e5c3cfe8de45e4a1a962f0020e7aad7ab
Subotic <S7386B@corp1.ad1.seb.net>
2020-07-15 16:22:32 +0800

    docs(datepicker): adds controls

--- projects/docs/src/app/docs-wrapper/pages/datepicker-page/datepicker-page.component.ts
+++ projects/docs/src/app/docs-wrapper/pages/datepicker-page/datepicker-page.component.ts
@@ -6,12 +6,24 @@ import { Component, OnInit } from "@angular/core";
 })
 export class DatepickerPageComponent implements OnInit {
     importString: string = require("!raw-loader!@sebgroup/ng-components/datePicker/date-picker.component");
-    model: Date;
     snippet: string = `<sebng-date-picker [(ngModel)]="model"></sebng-date-picker>`;
+    model: Date;
+
+    // controls
+    monthPicker: boolean;
+    forceCustom: boolean;
+    localeCode: string = "en-US";
+    rangeFrom: Date;
+    rangeTo: Date;
 
     constructor() {
         document.title = "Datepicker - SEB Angular Components";
-        this.model = new Date();
+        const today: Date = new Date();
+        const nextMonth: Date = new Date();
+        nextMonth.setMonth(today.getMonth() + 1);
+        this.model = today;
+        this.rangeFrom = today;
+        this.rangeTo = nextMonth;
     }
 
     ngOnInit(): void {}
