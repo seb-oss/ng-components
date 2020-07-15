53bba82d3bfa2443307a9ad12b7a3cfd24258027
Subotic <S7386B@corp1.ad1.seb.net>
2020-07-15 17:08:46 +0800

    docs(loader): adds controls for loader

--- projects/docs/src/app/docs-wrapper/pages/loader-page/loader-page.component.ts
+++ projects/docs/src/app/docs-wrapper/pages/loader-page/loader-page.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from "@angular/core";
+import { LoaderSize, LoaderType } from "@sebgroup/ng-components/loader";
+
+type Color = "primary" | "secondary" | "warning" | "danger" | "success" | "white" | "info" | "light" | "dark" | "body" | "muted";
+
+interface Item<T> {
+    label: string;
+    value: T;
+    key: T;
+}
 
 @Component({
     selector: "app-loader-page",
@@ -14,4 +23,44 @@ export class LoaderPageComponent implements OnInit {
     }
 
     ngOnInit(): void {}
+
+    classNames: string;
+    lightColors: Array<Color> = ["white", "secondary", "light"];
+
+    size: Item<LoaderSize> = { value: "md", key: "md", label: "Medium (md)" };
+    type: Item<LoaderType> = { value: "spinner", key: "spinner", label: "Spinner" };
+    color: Item<Color>;
+    showText: Array<Item<boolean>> = [];
+    showTextList: Array<Item<boolean>> = [{ value: false, key: false, label: "show text" }];
+
+    sizeList: Array<Item<LoaderSize>> = [
+        { value: "xs", key: "xs", label: "Extra small (xs)" },
+        { value: "sm", key: "sm", label: "Small (sm)" },
+        { value: "md", key: "md", label: "Medium (md)" },
+        { value: "lg", key: "lg", label: "Large (lg)" },
+    ];
+
+    typeList: Array<Item<LoaderType>> = [
+        { value: "spinner", key: "spinner", label: "Spinner" },
+        { value: "square", key: "square", label: "Square" },
+    ];
+
+    colorList: Array<Item<Color>> = [
+        { value: undefined, key: undefined, label: "none" },
+        { value: "primary", key: "primary", label: "text-primary" },
+        { value: "secondary", key: "secondary", label: "text-secondary" },
+        { value: "warning", key: "warning", label: "text-warning" },
+        { value: "danger", key: "danger", label: "text-danger" },
+        { value: "success", key: "success", label: "text-success" },
+        { value: "white", key: "white", label: "text-white" },
+        { value: "info", key: "info", label: "text-info" },
+        { value: "light", key: "light", label: "text-light" },
+        { value: "dark", key: "dark", label: "text-dark" },
+        { value: "body", key: "body", label: "text-body" },
+        { value: "muted", key: "muted", label: "text-muted" },
+    ];
+
+    getSvgColor(): string {
+        return this.lightColors.some((c: Color) => c === this.color?.value) ? "#636363" : "#efefef";
+    }
 }
--- projects/docs/src/app/docs-wrapper/pages/loader-page/loader-page.module.ts
+++ projects/docs/src/app/docs-wrapper/pages/loader-page/loader-page.module.ts
@@ -5,12 +5,24 @@ import { CommonModule } from "@angular/common";
 import { Routes, RouterModule } from "@angular/router";
 import { DocPageModule } from "../../doc-page/doc-page.module";
 import { FormsModule } from "@angular/forms";
+import { CheckBoxModule } from "@sebgroup/ng-components/checkBox";
+import { DropdownModule } from "@sebgroup/ng-components/dropdown";
+import { RadioGroupModule } from "projects/ng-components/public-api";
 
 const routes: Routes = [{ path: "", component: LoaderPageComponent }];
 
 @NgModule({
     declarations: [LoaderPageComponent],
-    imports: [CommonModule, RouterModule.forChild(routes), FormsModule, DocPageModule, LoaderModule],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+        FormsModule,
+        DocPageModule,
+        LoaderModule,
+        CheckBoxModule,
+        DropdownModule,
+        RadioGroupModule,
+    ],
     exports: [RouterModule, LoaderModule],
 })
 export class LoaderPageModule {}
