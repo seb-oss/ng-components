151e0a269f8b83cf6ab79589674476f5be559d11
Chang <S2721C@corp1.ad1.seb.net>
2020-07-15 10:56:05 +0800

    docs(timer): amend timer doc

--- projects/docs/src/app/docs-wrapper/pages/timer-page/timer-page.component.ts
+++ projects/docs/src/app/docs-wrapper/pages/timer-page/timer-page.component.ts
@@ -5,9 +5,23 @@ import { Component, OnInit } from "@angular/core";
     template: `
         <app-doc-page [importString]="importString">
             <ng-container example>
-                <sebng-timer duration="90000"></sebng-timer>
+                <sebng-timer [duration]="duration" (onTimerEnd)="isToggled = true"></sebng-timer>
             </ng-container>
-            <ng-container controls> </ng-container>
+            <div class="row" controls>
+                <label class="m-3 col-12">
+                    duration
+                    <p><small>set duration of timer</small></p>
+                    <input class="form-control" [(ngModel)]="duration" />
+                </label>
+                <sebng-notification
+                    [toggle]="isToggled"
+                    title="Time is up"
+                    message="Timer ended"
+                    [dismissable]="true"
+                    [dismissTimeout]="2000"
+                    (onDismiss)="isToggled = false"
+                ></sebng-notification>
+            </div>
             <ng-container code>{{ snippet }}</ng-container>
         </app-doc-page>
     `,
@@ -15,6 +29,9 @@ import { Component, OnInit } from "@angular/core";
 export class TimerPageComponent implements OnInit {
     importString: string = require("!raw-loader!@sebgroup/ng-components/timer/timer.component");
     snippet: string = `<sebng-timer duration="90000"></sebng-timer>`;
+
+    duration: number = 90000;
+    isToggled: boolean = false;
 
     constructor() {
         document.title = "Timer - SEB Angular Components";
