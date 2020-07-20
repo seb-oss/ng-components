658b212685c06574eaf1454296370da92a9989b2
Chang <S2721C@corp1.ad1.seb.net>
2020-07-20 17:06:36 +0800

    feat(portal): add base portal

--- projects/ng-components/lib/portal/base-portal.component.ts
+++ projects/ng-components/lib/portal/base-portal.component.ts
@@ -6,6 +6,7 @@ export interface PortalWrapper<T = any> {
 
 export abstract class BasePortal<T> {
     private _host: PortalWrapper;
+    private _hostElement: HTMLDivElement;
 
     attach(host: PortalWrapper<T>): T {
         if (host === null) {
@@ -32,5 +33,20 @@ export abstract class BasePortal<T> {
 
     setHost(host: PortalWrapper): void {
         this._host = host;
+    }
+
+    getHost(hostId?: string): HTMLDivElement {
+        return this._hostElement || this.createHost(hostId);
+    }
+
+    createHost(hostId?: string): HTMLDivElement {
+        let outletElement: HTMLDivElement = document.querySelector(`#${hostId}`);
+        if (!outletElement) {
+            const newDivElement: HTMLDivElement = document.createElement("div");
+            document.body.append(newDivElement);
+            outletElement = newDivElement;
+        }
+        this._hostElement = outletElement;
+        return this._hostElement;
     }
 }
--- projects/ng-components/lib/portal/template-portal.component.ts
+++ projects/ng-components/lib/portal/template-portal.component.ts
@@ -11,16 +11,20 @@ import {
     ViewContainerRef,
     AfterViewInit,
     OnDestroy,
+    ViewChild,
 } from "@angular/core";
 import { BasePortal } from "./base-portal.component";
 
 /** Template portal is a component where allow */
 @Component({
     selector: "sebng-template-portal",
+    template: `<ng-template #portalTemplate><ng-content></ng-content></ng-template>`,
     encapsulation: ViewEncapsulation.None,
 })
 export class TemplatePortalComponent<T> extends BasePortal<EmbeddedViewRef<T>> implements AfterViewInit, OnDestroy {
+    @Input() hostId: string;
     @Input() templateRef: TemplateRef<T>;
+    @ViewChild("portalTemplate") portalTemplateRef: TemplateRef<any>;
     private viewRef: EmbeddedViewRef<T>;
 
     constructor(private viewContainerRef: ViewContainerRef) {
@@ -28,8 +32,13 @@ export class TemplatePortalComponent<T> extends BasePortal<EmbeddedViewRef<T>> i
     }
 
     ngAfterViewInit(): void {
+        if (!this.templateRef) {
+            this.templateRef = this.portalTemplateRef;
+        }
         this.viewRef = this.viewContainerRef.createEmbeddedView(this.templateRef);
         this.viewRef.detectChanges();
+
+        this.attachToHost();
     }
 
     ngOnDestroy(): void {
@@ -37,5 +46,10 @@ export class TemplatePortalComponent<T> extends BasePortal<EmbeddedViewRef<T>> i
         if (index !== -1) {
             this.viewContainerRef.remove(index);
         }
+    }
+
+    attachToHost(): void {
+        // attach the view to the DOM element that matches our selector
+        this.viewRef.rootNodes.forEach(rootNode => this.getHost(this.hostId).appendChild(rootNode));
     }
 }
