af4cd8a5983ba3d519554aa60934ea6e735b9ed1
kp <kayphay92@gmail.com>
2020-07-27 17:18:36 +0800

    docs(doc): fix scully base href

--- scully.docs.config.ts
+++ scully.docs.config.ts
@@ -1,7 +1,14 @@
-import { ScullyConfig } from "@scullyio/scully";
+import { ScullyConfig, setPluginConfig } from "@scullyio/scully";
+import { baseHrefRewrite } from "@scullyio/scully-plugin-base-href-rewrite";
+
+const pkg = require("./package.json");
+const defaultPostRenderers = [baseHrefRewrite];
+setPluginConfig(baseHrefRewrite, { href: pkg.homepage });
+
 export const config: ScullyConfig = {
     projectRoot: "./projects/docs/src",
     projectName: "docs",
     outDir: "./dist/static",
+    defaultPostRenderers,
     routes: {},
 };
