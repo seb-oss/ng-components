a7779150c8178d155f96fa66af6d927f3cc08417
Chang <S2721C@corp1.ad1.seb.net>
2020-07-27 14:41:55 +0800

    docs(doc): fix scully doc

--- projects/docs/src/app/app-routing.module.ts
+++ projects/docs/src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
     { path: "", loadChildren: () => import("./home/home.module").then(m => m.HomeModule) },
-    // { path: "docs", loadChildren: () => import("./docs-wrapper/docs-wrapper.module").then(m => m.DocsWrapperModule) },
-    // { path: "**", loadChildren: () => import("./common/not-found/not-found.module").then(m => m.NotFoundModule) },
+    { path: "docs", loadChildren: () => import("./docs-wrapper/docs-wrapper.module").then(m => m.DocsWrapperModule) },
+    { path: "404", loadChildren: () => import("./common/not-found/not-found.module").then(m => m.NotFoundModule) },
+    { path: "**", loadChildren: () => import("./common/not-found/not-found.module").then(m => m.NotFoundModule) },
 ];
 
 @NgModule({
