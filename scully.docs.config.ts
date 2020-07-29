9450c00181a31d165b3401927ba9a91a964128f2
kp <kayphay92@gmail.com>
2020-07-29 14:45:06 +0800

    docs(docs): add in seo meta tag
    add in seo optimzation, rename folder

--- scully.docs.config.ts
+++ scully.docs.config.ts
@@ -2,13 +2,13 @@ import { ScullyConfig, setPluginConfig } from "@scullyio/scully";
 import { baseHrefRewrite } from "@scullyio/scully-plugin-base-href-rewrite";
 
 const pkg = require("./package.json");
-const defaultPostRenderers = [baseHrefRewrite];
+const defaultPostRenderers = ["seoHrefOptimise", baseHrefRewrite];
 setPluginConfig(baseHrefRewrite, { href: pkg.homepage });
 
 export const config: ScullyConfig = {
     projectRoot: "./projects/docs/src",
     projectName: "docs",
-    outDir: "./dist/static",
+    outDir: "./dist/docs",
     defaultPostRenderers,
     routes: {},
 };
