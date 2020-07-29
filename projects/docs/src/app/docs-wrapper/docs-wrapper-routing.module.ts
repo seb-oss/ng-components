1b3b0a1b34eaefc5245ba54696af1b228872f87e
kp <kayphay92@gmail.com>
2020-07-29 09:15:55 +0800

    docs(docs): remove redundant function

--- projects/docs/src/app/docs-wrapper/docs-wrapper-routing.module.ts
+++ projects/docs/src/app/docs-wrapper/docs-wrapper-routing.module.ts
@@ -3,17 +3,6 @@ import { Routes, RouterModule } from "@angular/router";
 import { DocsWrapperComponent } from "./docs-wrapper.component";
 import components from "../../assets/components-list.json";
 
-function getComponentPageRoutes(): Routes {
-    return components.map(({ path, filePath, module }: ComponentsListItem) => {
-        const component: string = path.replace("/docs/", "");
-        return {
-            path: component,
-            // This is the only way to trigger Angular to pre-process the file path import(`${filePath})
-            loadChildren: () => import(`${filePath}`).then(m => m[module]),
-        };
-    });
-}
-
 const routes: Routes = [
     {
         path: "",
