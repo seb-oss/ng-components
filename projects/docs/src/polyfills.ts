037440fab7106915dcea09ac91ef42319fb1602f
Boudfoust <S8381B@corp1.ad1.seb.net>
2021-03-04 20:34:00 +0800

    fix: fix documentation on IE11

--- projects/docs/src/polyfills.ts
+++ projects/docs/src/polyfills.ts
@@ -17,7 +17,7 @@
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
-
+import "new-event-polyfill";
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 import "classlist.js"; // Run `npm install --save classlist.js`.
 
