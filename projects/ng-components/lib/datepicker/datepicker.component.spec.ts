99b20dbdeb62b39b2a3407c130bc3a1f944c0055
Kher Phay Chang <kayphay92@gmail.com>
2021-12-14 13:05:14 +0800

    fix date range

--- projects/ng-components/lib/datepicker/datepicker.component.spec.ts
+++ projects/ng-components/lib/datepicker/datepicker.component.spec.ts
@@ -30,7 +30,7 @@ class CustomTestClass {
         const today: Date = new Date();
         this.dateValue = today;
         this.startDate = new Date(today.getFullYear(), 0, today.getDate());
-        this.endDate = new Date(today.getFullYear(), 11, today.getDate());
+        this.endDate = new Date(today.getFullYear(), 12, today.getDate());
 
         this.id = "my-datepicker";
         this.className = "my-class";
